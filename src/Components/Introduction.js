d7ebed527b62de460597487a3bf2cf722f117fab
Awaish-prog <awaishk640@gmail.com>
2023-03-03 21:59:24 +0530

    Added Intro animation

--- src/Components/Introduction.js
+++ src/Components/Introduction.js
@@ -1,11 +1,20 @@
 import "../CSS/Introduction.css"
+import {useEffect, useState} from "react"
 
 export default function Introduction(){
+    const [role, setRole] = useState("Full-stack Web Developer")
+
+    function changeRole(){
+        setRole((prev) => {
+            return prev === "Full-stack Web Developer" ? "Coding Instructor" : "Full-stack Web Developer"
+        })
+    }
+    
     return (
         <section className="introduction">
             <div className="intro-title">
                 <h1>I'm Awaish Khan</h1>
-                <span className="roleContainer"><h3 className="role">Full-stack</h3></span>
+                {<span className="roleContainer"><h3 className="role" onAnimationIteration={changeRole}>{role}</h3></span>}
             </div>
         </section>
     )
--- src/Components/Project.js
+++ src/Components/Project.js
@@ -5,7 +5,7 @@ import demo from "../Images/demo.png"
 export default function Project({project}){
     return (
     <div className="project">
-        <span><img src={project.image} width="50px" /></span>
+        {/* <span><img src={project.image} width="50px" /></span> */}
         <p className="project-name">{project.name}</p>
         <p className="project-des">{project.des}</p>
         <h3>Technologies used</h3>
